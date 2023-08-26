0a24acbed2fc5dd4bdf459e6aa335bba84a636d2
Lifebed <juliangaviri1216@gmail.com>
2023-08-26 17:07:15 -0500

    commit 2

---
+++ app.py
@@ -0,0 +1,32 @@
+def ejecicio1():
+    print("Ejercicio del ejecicio 1")
+def ejecicio2():
+    print("Ejercicio del ejecicio 2")
+def ejecicio3():
+    print("Ejercicio del ejecicio 3")
+def ejecicio4():
+    print("Ejercicio del ejecicio 4")
+def ejecicio5():
+    print("Ejercicio del ejecicio 5")
+
+
+
+print("Menu")
+print("1. Ejercicio 1")
+print("2. Ejercicio 2")
+print("3. Ejercicio 3")
+print("4. Ejercicio 4")
+print("5. Ejercicio 5")
+
+opcion = int(input("Seleccione una opcion: "))
+
+if opcion == 1:
+    ejecicio1()
+elif opcion == 2:
+    ejecicio2() 
+elif opcion == 3:
+    ejecicio3() 
+elif opcion == 4:
+    ejecicio4()
+elif opcion == 5:
+    ejecicio5() 
